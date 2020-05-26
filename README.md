de6f3bbad1071bae161ba40e6391957badd1281f
Mark Young <markyaaronyoung@gmail.com>
2020-05-25 21:34:48 -0500

    finished ReadMe Markdown & worked on creating a student object from my addStudent and buildStudentCards function

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Sorting-Hat
+# Description
+Sorting-Hat is a project exercise that uses bootstrap forms, print to DOM functions, and click events. It takes a name input and uses a sorting function to parse it into one of four random houses based on the world of Harry Potter.
+
+# Getting Started
+
+1. Clone down repo
+
+2. Launch project using your preferred serve (I use http: https://www.npmjs.com/package/http-server)
+
+3. Choose a browser (such as https://www.google.com/chrome/) 
+
+4. Using your browser, go to address where project will be served (such as localhost:8080)
+
+# Contributors 
+* [Mark A. Young](https://github.com/MarkyAaronYoung)
+
+# Feature Request/TODO
+- [x] A [bootstrap jumpbotron](https://getbootstrap.com/docs/4.0/components/jumbotron/) to introduces the sorting hat
+- [x] A [bootstrap form](https://getbootstrap.com/docs/4.1/components/forms/) is employed for "students" to fill in their names
+- [x] Submitting form sorts student names into one of four houses at random (Gryffindor, Hufflepuff, Ravenclaw, or Slytherin) and returns name and random house on a [bootstrap](https://getbootstrap.com/docs/4.1/components/card/)
+- [ ] An expel button on the card should hide/remove the student card 
+
+# Screenshots
+![Sorting Hat Jumbotron](Images/Screen Shot 2020-05-25 at 8.53.58 PM.png)
+
+# Code Block Example
+```
+// function that creates a card by looping over the new student array created by the addStudent function
+const buildStudentCards = (studentArray) => {
+  let domString = ''; 
+  for (let i = 0; i < studentArray.length; i++){
+    domString += `
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${studentArray[i]}</h5>
+          <p class="card-text">${sortedHouse()}</p>
+          <a href="#" class="btn btn-primary">Expel</a>
+        </div>
+      </div>
+    `;
+  };
+  console.log('do I work?')
+  printToDom('#studentCard', domString);
+}
--- main.js
+++ main.js
@@ -1,67 +1,103 @@
+// I want to create a student array so I can add new Hogwart students into as objects
+// They have to be assigned to one of four houses that are constants
+// I will need to create an array of expelled students
 let students = [];
-
 const houses = [
-  {
-    house: 'gryffindor',
-  },
-  {
-    house: 'hufflepuff',
-  },
-  {
-    house: 'ravenclaw',
-  },
-  {
-    house: 'slytherin'
-  }
+  'gryffindor',
+  'hufflepuff',
+  'ravenclaw',
+  'slytherin'
 ]
-
 let expelledStudents = [];
-
 // This is my print to dom function
 const printToDom = (selector, textToPrint) => {
   const selectedDiv = document.querySelector(selector);
   selectedDiv.innerHTML = textToPrint;
 }
- //function to add value of button id #sortButton to student array/ This won't console.log out.
-  const studentName = document.getElementById('studentName').value;
-  console.log(studentName);
- 
-  const sortedHouse = houses[Math.floor(Math.random() * 4)].house
-  console.log(sortedHouse);
 
+const sortingHat = () =>{
+
+  sortedHouse =  (Math.floor(Math.random() * 4))
+  // console.log(sortedHouse)
+  switch (sortedHouse) {
+  case 0:
+    return("Gryffindor");
+    break;
+  case 1:
+    return ("Slytherin");
+    break;
+  case 2:
+    return ("Hufflepuff");
+    break;
+  case 3:
+    return ("Ravenclaw");
+    break;
+  }
+}
+
+const sortedStudent = [];
+// console.log(sortedStudent);
+const sortStudent = (event) => {
+  // console.log(sortedStudent);
+  let student = {};
+
+  if (document.querySelector('#nameInput').value === "" ) {
+    alert("FILL IN YOUR NAME");
+    return
+  }
+  student.name = document.querySelector('#nameInput').value;
+  student.house = sortingHat();
+  student.unique = Math.floor(Math.random() * 100);
+  sortedStudent.push(student);
+
+  document.querySelector('#nameInput').value="";
+
+  printedCard();
+}
+
+ //function to add value of button id #sortButton to student array
+// const addStudent = () => {
   
-  
+//   console.log('addStudent')
+//   const studentName = document.getElementById('nameInput').value;
+//   console.log(studentName);
+//   students.push(studentName);
+//   buildStudentCards(students);
+//   console.log(students); 
+
+// }
+// document.querySelector('#sortButton').addEventListener('click', addStudent);
+//   const sortedHouse = () => {
+//     const houseIndex = Math.floor((Math.random() * 4));
+//     console.log('house index', houseIndex);
+//     const studentHouse = houses[houseIndex];
+//     return studentHouse;
+// };
 
 
+  // console.log(sortedHouse);
 // function that creates a card by looping over the new student array created by the addStudent function
-
 const buildStudentCards = (studentArray) => {
   let domString = ''; 
-  
-
   for (let i = 0; i < studentArray.length; i++){
-  
     domString += `
       <div class="card" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">${studentArray[i].name}</h5>
-          <p class="card-text">${sortedHouse}</p>
+          <h5 class="card-title">${studentArray[i]}</h5>
+          <p class="card-text">${sortedHouse()}</p>
           <a href="#" class="btn btn-primary">Expel</a>
         </div>
       </div>
     `;
   };
-  printToDom('#studentCard', domString);
   console.log('do I work?')
+  printToDom('#studentCard', domString);
 }
-
 // click event when the button with id = sortButton is pushed
 const clickEvents = () => {
-    document.querySelector('#sortButton').addEventListener('click', buildStudentCards);
 }
-
 // init function to call buildStudentCards and clickEvent
 const init = () => {
-  buildStudentCards(students);
-  clickEvents();
+  // clickEvents();
 };
+// init();
